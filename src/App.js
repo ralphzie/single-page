0b4678a01971094c29f007da81cedfa9e540648c
ralphzie <ubyraf@gmail.com>
2023-02-07 10:40:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+
+
+  return(
+      <div className="main">
+           <div className="bg-image">
+
+           </div>
+           <div className="form bg-white">
+            <form> 
+              <h1>Register to Order!</h1>
+            <div class="mb-3">
+              <input type="email" class="form-control" id="fullname" placeholder="fullname"/>
+            </div>
+            <div class="mb-3">
+              <input type="email" class="form-control" id="email" placeholder="name@example.com"/>
+            </div>
+            <div class="mb-3">
+              <input type="email" class="form-control" id="description" placeholder="description"/>
+            </div>
+            <div class="mb-3">
+              <input type="email" class="form-control" id="location" placeholder="name@example.com"/>
+            </div>
+            <div class="mb-3">
+            <select class="form-select" aria-label="Default select example">
+                    <option selected>select country</option>
+                    <option value="UK1">UK</option>
+                    <option value="USA">USA</option>
+                    <option value="SA">S.A</option>
+            </select>
+            </div>
+            <div class="mb-3">
+            <label for = "gender"> Gender: </label>
+
+            <input className="mx-4" type= "radio" id="male"></input>
+            <label for = "male"> Male </label>
+
+            <input className="mx-4" type= "radio" id="female"></input>
+            <label for = "female"> female </label>
+
+            <input className="mx-4" type= "radio" id="other"></input>
+            <label for = "other"> other </label> 
+
+            </div>
+
+           <div class="mb-3">
+              <label for = "agree"> Agree: </label>
+              <input className="mx-4" type= "checkbox" id="agree"></input>
+            </div>
+
+            <div class="mb-3">
+              <button className="btn btn-md btn-success">submit</button>
+            </div>
+            </form>
+           </div>
+       </div>
+  )
+    
 }
 
 export default App;
+
+
+
